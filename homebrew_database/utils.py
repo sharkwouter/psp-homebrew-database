54cd5f96923743d7a2f9fc3b51a92a979351efc5
Wouter Wijsman <wouter.wijsman@rewe-group.com>
2026-09-18 10:38:37 +0200

    Fix relative path conversion

--- homebrew_database/utils.py
+++ homebrew_database/utils.py
@@ -11,4 +11,4 @@ def relative_to_absolute_url(url: str | None) -> str | None:
         return url
     
     owner,repo = os.environ["GITHUB_REPOSITORY"].split("/")
-    return str(urllib.parse.urljoin(f"https://{owner}.github.io/{repo}", url))
+    return str(urllib.parse.urljoin(f"https://{owner}.github.io/{repo}/", url))
